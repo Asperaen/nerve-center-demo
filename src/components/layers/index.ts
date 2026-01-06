eb725064c9cf5acf7f1d7c83b309b04fc7e9d0e3
Yining Tao <Yining_Tao@mckinsey.com>
2026-01-06 12:34:38 +0800

    fix src/components/layers/index.ts  error

--- src/components/layers/index.ts
+++ src/components/layers/index.ts
@@ -1,3 +1,3 @@
 export { default as ProductAnalysisLayer } from './ProductAnalysisLayer';
-// CostImpactBreakdownLayer and MVABreakdownLayer are now integrated into ProductAnalysisLayer
-// and are no longer exported as separate layers
+export { default as CostImpactBreakdownLayer } from './CostImpactBreakdownLayer';
+export { default as MVABreakdownLayer } from './MVABreakdownLayer';
